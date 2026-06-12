24d0b438234c31856ae30042d5f92c90062ac7c4
Amansinghh08 <Aman35u2@gmail.com>
2026-06-13 01:16:57 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const apiRoutes = require('./routes/apiRoutes');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+const path = require('path');
+const { default: mongoose } = require('mongoose');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+ let isconnected = false;
+
+ async function connectToMongoDB(){
+    try{
+        await mongoose.connect(process.evn.MONGO_URI,{
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+        isconnected = true;
+        console.log('Connected to MongoDB');
+    }catch(error){
+        console.error('Error connecting to MongoDB:', error);
+         setTimeout(connectToMongoDB, 5000); // Retry after 5 seconds 
+        }
+
+        
+
+    }
+
+ // add middleware
+ aap.use((req, res, next) => {
+    if (!isConnected){
+        connectToMongoDB();
+    }
+next();
+ })
+
+
+// Routes
+app.use('/api', apiRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app
